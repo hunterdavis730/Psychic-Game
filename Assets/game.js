ac7d5c532b28f450c5691961b8c12b0348f007c4
Hunter Davis <hunterdavis730@gmail.com>
2019-02-21 13:27:26 -0700

    Second Commit

--- Assets/game.js
+++ Assets/game.js
@@ -1,20 +1,27 @@
 const computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const userChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 var wins = 0;
 var losses = 0;
 
-const lettersGuessed = [];
+var lettersGuessed = [];
 
 var guessesLeft = 8;
 
-const computerLetter = computerChoices[(Math.floor(Math.random() * computerChoices.length))];
+var computerLetter = computerChoices[(Math.floor(Math.random() * computerChoices.length))];
 
+var pickLetter = function () {
+    return computerLetter = computerChoices[(Math.floor(Math.random() * computerChoices.length))];
+}
 
 document.onkeyup = function (event) {
     const userGuess = event.key.toLowerCase();
 
+
+
     document.getElementById('remaining').textContent = `Guesses Left: ${guessesLeft}`;
+
+    document.getElementById('winLine').textContent = `Wins: ${wins}`;
+    document.getElementById('lossLine').textContent = `Losses: ${losses}`;
 
 
     for (i = 0; lettersGuessed.length < 9; i++) {
@@ -22,34 +29,33 @@ document.onkeyup = function (event) {
 
         guessesLeft--;
         lettersGuessed.push(userGuess);
-        console.log(guessesLeft)
-        console.log(lettersGuessed.length)
+        console.log(computerLetter)
+        console.log(wins);
 
+        if (userGuess === computerLetter) {
+            wins++;
+            lettersGuessed = [];
+            alert('That is right! I was thinking of ' + computerLetter.toString().toUpperCase());
+            pickLetter();
+            return guessesLeft = 8;
 
-
-        if (guessesLeft === -1) {
+        } else if (guessesLeft === -1) {
             document.getElementById('Guesses').textContent = `Your Guesses so Far: ${lettersGuessed.toString(' ')}  `;
-
-
-
-            return confirm('Game Over. Play Again?');
+            losses++;
+            var gameOver = confirm('Game Over. Play Again?');
 
         } else {
             return document.getElementById('Guesses').textContent = `Your Guesses so Far: ${lettersGuessed.toString(' ')}  `;
+        };
+
+        if (gameOver === true) {
+            lettersGuessed = [];
+            pickLetter();
+            return guessesLeft = 8;
         }
 
 
 
-
-
-
-
     }
-
-
-
-
-
-
-
+    console.log(wins);
 }
